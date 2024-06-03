840277176f8e1b11d1929e5d05c7d4d969db6cbb
Antonije Erdeljan <antonijeerdeljan01@gmail.com>
2024-06-03 19:57:05 +0200

    Zipkin

--- src/Explorer.API/Program.cs
+++ src/Explorer.API/Program.cs
@@ -2,18 +2,57 @@ using Explorer.API.Controllers.Proto;
 using Explorer.API.Middleware;
 using Explorer.API.Startup;
 using Microsoft.Extensions.FileProviders;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Context.Propagation;
+using System.Diagnostics;
+using OpenTelemetry.Instrumentation.Process;
 
 var builder = WebApplication.CreateBuilder(args);
-
 
 builder.Services.AddControllers();
 builder.Services.ConfigureSwagger(builder.Configuration);
 const string corsPolicy = "_corsPolicy";
 builder.Services.ConfigureCors(corsPolicy);
 builder.Services.ConfigureAuth();
-
 builder.Services.RegisterModules();
 builder.Services.AddGrpc();
+
+const string serviceName = "api";
+
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options
+        .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName))
+        .AddConsoleExporter();
+});
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService(serviceName))
+    .WithTracing(tracing => tracing
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddEntityFrameworkCoreInstrumentation(options =>
+        {
+            options.SetDbStatementForText = true;
+        })
+        .AddConsoleExporter()
+        .AddZipkinExporter(zipkinOptions =>
+        {
+            zipkinOptions.Endpoint = new Uri("http://localhost:9411/api/v2/spans");
+        })
+        .AddJaegerExporter(jaegerOptions =>
+        {
+            jaegerOptions.AgentHost = "localhost";
+            jaegerOptions.AgentPort = 6831;
+        }))
+    .WithMetrics(metrics => metrics
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddRuntimeInstrumentation()
+        .AddConsoleExporter());
 
 var app = builder.Build();
 
